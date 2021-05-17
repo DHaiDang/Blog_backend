ea0b195493b8cfde63d6f9fedc88d49afa558fa2
dangdh16 <50817453+DHaiDang@users.noreply.github.com>
2021-05-17 22:47:07 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,41 @@
-# Script run "Mì ăn liền"
-docker build -t app/service:v1 .
-docker run --rm -v $(pwd):/app -w /app node:13-alpine npm install
-docker-compose up
+#!/usr/bin/env bash
+echo 
+"
+                  ####        .            
+              ### ####       ==            
+          ### ### ####      ===            
+       /###############\___/ ===        
+  ~~~ {~~ ~~~~ ~~~ ~~~~ ~~ ~ /  ===-  
+       \______ o          __/            
+         \    \        __/             
+          \____\______/                
+ 
+          |          |
+       __ |  __   __ | _  __   _
+      /  \| /  \ /   |/  / _\ | 
+      \__/| \__/ \__ |\_ \__  |
+
+"
+
+
+if [ "$(uname)" == "Darwin" ]; then
+    echo 'detect your machine is Macos ~ !'
+    docker build -t app/service:v1 .
+    docker run --rm -v $(pwd):/app -w /app node:13-alpine npm install
+    docker-compose up
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    echo 'detect your machine is Linux ~ !'
+    docker build -t app/service:v1 .
+    docker run --rm -v $(pwd):/app -w /app node:13-alpine npm install
+    docker-compose up
+elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
+    echo 'detect your machine is Window ~ !'
+    docker build -t app/service:v1 .    
+    docker run --rm -v "/$(pwd)":/app -w //app node:13-alpine npm install window
+    docker-compose up
+elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
+    echo 'detect your machine is Window'
+    docker build -t app/service:v1 .
+    docker run --rm -v "/$(pwd)":/app -w //app node:13-alpine npm install window
+    docker-compose up
+fi
