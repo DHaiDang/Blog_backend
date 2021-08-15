25c2d7a548746422db7892efdb595de375e47273
dangdh16 <donghaidang179@gmail.com>
2021-08-16 02:28:56 +0700

    DONE mode admin

--- controllers/api_blog_controller.js
+++ controllers/api_blog_controller.js
@@ -1,9 +1,61 @@
 const Posts = require('../models/Posts')
 
 class api_blog_posts {
-    static form (req, res, next) {
-        console.log("Hello")
-        res.render('posts.ejs')
+    static async index (req, res, next) {
+        console.log("Index")
+        try {
+            const posts = await Posts.find({})
+            return res.render('index.ejs',{
+                data : posts
+            })
+        } catch (error) {
+            return res.status(200).send({ Message: 'Error', error})
+        }
+    }
+    static create (req, res, next) {
+        res.render('create.ejs')
+    }
+    static async update (req, res, next) {
+        let id = req.query.id
+        try {
+            let posts_update = await Posts.find({}).where("_id").equals(id)
+            console.log(posts_update)
+            console.log(req.body.id)
+            return res.render('update.ejs', {
+                data : posts_update
+            })
+        } catch (error) {
+            console.log(error)
+            return res.status(200).send({ Message: 'Error', error})
+        }
+    }
+    static async update_post (req, res, next) {
+        console.log("update post" + req.body.id)
+        let posts = {
+            title: req.body.title,
+            summary: req.body.summary,
+            category:req.body.category,
+            author: req.body.author,
+            content:req.body.content,
+            publish: Date.now(),
+            image:req.body.summary,
+        }
+        await Posts.findOneAndUpdate(req.body.id, posts, {upsert: true}, function(err, doc) {
+            if (err){
+                return res.status(200).send({ Message: 'Error', error})
+            }
+            return res.redirect('/api')
+        });
+    }
+    static async delete (req, res, next) {
+        let id = req.body.id
+        console.log(req.body.id)
+        await Posts.deleteOne({ _id: id}, function (err) {
+            if (err) {
+                return res.status(200).send({ Message: 'Error', error})
+            }
+            res.redirect('/api')
+          });
     }
     static async list (req, res, next) {
         async function query_posts(by, query) {
@@ -31,12 +83,13 @@ class api_blog_posts {
             }
         } 
     }
-    static async post (req, res, next) {
+    static async create_new_post (req, res, next) {
         try {
             let posts = {
                 title: req.body.title,
                 summary: req.body.summary,
                 category:req.body.category,
+                author: req.body.author,
                 content:req.body.content,
                 publish: Date.now(),
                 image:req.body.summary,
@@ -52,7 +105,8 @@ class api_blog_posts {
                     return err
                 }
                 else {
-                    return res.status(200).send({ Message: 'Success'})
+                    return res.redirect('/api')
+                    //return res.status(200).send({ Message: 'Success'})
                 }
             })
         } catch (error) {
--- routes/api_blog.js
+++ routes/api_blog.js
@@ -4,10 +4,19 @@ const middlewares = require('../middlewares')
 
 const api_blog_controller = require('../controllers/api_blog_controller')
 
-router.get('/', api_blog_controller.form)
+router.get('/', api_blog_controller.index)
 
+//get
+router.get('/create', api_blog_controller.create)
+router.get('/update', api_blog_controller.update)
+
+//post
+router.post('/create', api_blog_controller.create_new_post)
+router.post('/update', api_blog_controller.update)
+router.post('/delete', api_blog_controller.delete)
+
+//api
 router.get('/list', api_blog_controller.list)
-router.post('/post', api_blog_controller.post)
 
 
 //router.get('/', middlewares.isAuthenticated, ProductController.index)
